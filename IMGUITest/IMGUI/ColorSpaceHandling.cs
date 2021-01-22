5d38438e7fefd5a44e4fd224df025a3ca307e2dd
Phoenix Aran <jadearan.iwnl@gmail.com>
2021-01-22 11:03:38 -0500

    Initial commit

---
+++ IMGUITest/IMGUI/ColorSpaceHandling.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMGUITest
+{
+    /// <summary>
+    /// Identifies the kind of color space handling that an <see cref="ImGuiController"/> uses.
+    /// </summary>
+    public enum ColorSpaceHandling
+    {
+        /// <summary>
+        /// Legacy-style color space handling. In this mode, the renderer will not convert sRGB vertex colors into linear space
+        /// before blending them.
+        /// </summary>
+        Legacy = 0,
+        /// <summary>
+        /// Improved color space handling. In this mode, the render will convert sRGB vertex colors into linear space before
+        /// blending them with colors from user Textures.
+        /// </summary>
+        Linear = 1,
+    }
+}
---
+++ IMGUITest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IMGUITest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IMGUITest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97639fd6-1fa6-45dd-8c9c-1beec27c0e3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
